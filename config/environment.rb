200bf556d6d2cf74b0be87395292a41fc896309c
michael hohl <mhohl@rocketmail.com>
2018-05-04 21:34:55 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -17,5 +17,5 @@ connection_details = YAML::load(File.open('config/database.yml'))
 DB = ActiveRecord::Base.establish_connection(connection_details)
 
 if ENV["ACTIVE_RECORD_ENV"] == "test"
-  ActiveRecord::Migration.verbose = true
+  ActiveRecord::Migration.verbose = false
 end
